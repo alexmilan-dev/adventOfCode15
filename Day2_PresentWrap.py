919e2e0bf85475381c1f7e14538408eb69cabe69
alexmilan_dev <alexmilan@gmail.com>
2022-12-01 22:59:13 +0100

    Versió 1.02

--- Day2_PresentWrap.py
+++ Day2_PresentWrap.py
@@ -1,3 +1,21 @@
+def calc_lower_perimeter(l,w,h):
+    perimeter_a = 2 * l + 2 * w
+    perimeter_b = 2 * w + 2 * h
+    perimeter_c = 2 * l + 2 * h
+
+    if (perimeter_a < perimeter_b):
+        if perimeter_a < perimeter_c:
+            return perimeter_a
+        else:
+            return perimeter_c
+    else:
+        if (perimeter_b < perimeter_c):
+            return perimeter_b
+        else:
+            return perimeter_c
+##print(calc_lower_perimeter(2,3,4))
+
+ribbon = 0
 surfacet = 0
 with open('Day2Input.txt') as f:
     for line in f:
@@ -22,4 +40,7 @@ with open('Day2Input.txt') as f:
         surface = (2 * l * w + 2 * w * h + 2 * l * h) + small_side_area
         surfacet = surfacet + surface
 
+        ribbon = ribbon + calc_lower_perimeter(l, w, h) + l*w*h
+
 print(surfacet)
+print(ribbon)
